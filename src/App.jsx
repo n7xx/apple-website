aa8c108e1037abe16b6a5f66ea4f40db09d7f6d1
n7xx <nvshvvt@gmail.com>
2025-07-27 18:55:05 +0300

    Please enter the commit message for your changes. Lines starting

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>the apple website before starting.
+        <br />
+        <span className='text-red-500'>wait a minute</span>
+      </h1>
+    </>
+  )
+}
+
+export default App
