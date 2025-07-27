eae9b9b4b82250cbef671afc53c7fb1563cdd5a4
nashaatfathy878@gmail.com <nashaatfathy878@gmail.com>
2025-07-27 16:22:08 +0300

    create a new commit with the following message:  "Add new assets and components for the project"

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+
+export default function App() {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  );
+}
